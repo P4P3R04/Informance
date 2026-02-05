c106c4dc703b6f125f91e04bd803cea982c0341f
P4P3R04 <saulrabadansandoval@gmail.com>
2026-02-05 14:04:03 +0100

    5/2/26

---
+++ project1/app/projects/page.tsx
@@ -0,0 +1,16 @@
+
+import Header from "../../components/Header";
+
+
+export default function Projects() {
+    return(
+        <>
+        <Header/>
+        <main>
+            <h1 className="text-center text-4xl font-bold">Projects</h1>
+            <p className="text-center m-2">Aquí puedes ver algunos de mis proyectos:</p>
+        </main>
+
+        </>
+    )
+}   
